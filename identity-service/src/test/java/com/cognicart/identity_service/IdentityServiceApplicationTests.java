e0dd58a5b65dd97aa1fa987f4e5bd4dc11f9623e
Sundarvikas01 <sundarme164@gmail.com>
2026-02-26 14:22:57 +0530

    Initialize Identity Service Spring Boot project

---
+++ identity-service/src/test/java/com/cognicart/identity_service/IdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognicart.identity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
